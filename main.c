73ebe7f9236d17d96aa9c5c8d2796ea90b3e016d
Rylee <Rylee@192.168.0.9>
2021-02-04 17:21:49 -0700

    Fixed bug to turn on LED1 Added delays to turn blink LED1

--- main.c
+++ main.c
@@ -1,14 +1,25 @@
 #include "msp.h"
 
-
 /**
  * main.c
  */
+
 void main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
 
-P1->DIR |= BIT0;
-P1->OUT &= ~BIT1;
+    P1->DIR |= BIT0; //Bit0 = 0001, set DIR to 0001, DIR = output
+
+    while(1){
+        int i;
+
+        for(i = 0; i < 150000; i++); //LED on delay
+            P1->OUT |= BIT0; //P1.0 high, LED off
+
+        for(i = 0; i < 150000; i++);//LED off delay
+            P1->OUT &= ~BIT0; //P1.0 low, LED off
+    }
+
 
 }
+
